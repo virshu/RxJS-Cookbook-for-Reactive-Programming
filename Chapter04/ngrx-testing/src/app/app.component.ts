be9ce8ae814aa48d51f24e0a77c7dea78abbca03
Nikola Mitrovic <nikolamitrovic@Nikolas-MacBook-Pro.local>
2025-03-28 03:39:06 +0100

    Chapter04 - NgRx testing - fix MSW.

--- Chapter04/ngrx-testing/src/app/app.component.ts
+++ Chapter04/ngrx-testing/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, CommonModule, RecipesListComponent, MatSidenavModule, SidebarComponent],
+    imports: [CommonModule, RecipesListComponent, MatSidenavModule, SidebarComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
