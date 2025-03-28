220848e6d0cf204276524225828961947ba0a22a
Nikola Mitrovic <nikolamitrovic@Nikolas-MacBook-Pro.local>
2025-03-28 03:23:40 +0100

    Chapter04 - marble testing - Angular v19 upgrade.

--- Chapter04/marble-testing/src/app/app.component.spec.ts
+++ Chapter04/marble-testing/src/app/app.component.spec.ts
@@ -1,16 +1,17 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RecipeService } from './services/recipe.service';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('AppComponent', () => {
   let service: RecipeService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, HttpClientTestingModule],
-      providers: [RecipeService]
-    }).compileComponents();
+    imports: [AppComponent],
+    providers: [RecipeService, provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+}).compileComponents();
     service = TestBed.inject(RecipeService);
   });
 
--- Chapter04/marble-testing/src/app/services/recipe.service.spec.ts
+++ Chapter04/marble-testing/src/app/services/recipe.service.spec.ts
@@ -1,15 +1,17 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { RecipeService } from './recipe.service';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('RecipeService', () => {
   let service: RecipeService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
-    });
+    imports: [],
+    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
+});
     service = TestBed.inject(RecipeService);
   });
 
